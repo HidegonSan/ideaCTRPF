382e119721afe31a03dbb30d2ee9cf6a274a50b3
kani537 <94041681+kani537@users.noreply.github.com>
2023-04-29 00:43:07 +0900

    v1.1.2

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -270,7 +270,7 @@ namespace CTRPluginFramework
 
   int main(void)
   {
-    PluginMenu *menu = new PluginMenu("IDEA CTRPF", 1, 1, 0, "made by kani537\n協力者(敬称略)\n・Tekito_256\n・hide0123\n・HidegonSan\n・bomkei\nありがとうございます！！！\n変な機能ばっかりの自己満ロマンCTRPFです");
+    PluginMenu *menu = new PluginMenu("IDEA CTRPF", 1, 1, 2, "made by kani537\n協力者(敬称略)\n・Tekito_256\n・hide0123\n・HidegonSan\n・bomkei\nありがとうございます！！！\n変な機能ばっかりの自己満ロマンCTRPFです");
 
     // OSD::Run(LoadGameTitle);
     // Sleep(Seconds(1));
